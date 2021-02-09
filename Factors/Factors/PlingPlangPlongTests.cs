b58fd1f187b38ac72999f5fc228545535307c2ef
SarkerJ <isarkerj@gmail.com>
2021-02-09 12:08:03 +0000

    added additional tests to cover most test cases

--- Factors/Factors/PlingPlangPlongTests.cs
+++ Factors/Factors/PlingPlangPlongTests.cs
@@ -4,9 +4,10 @@ namespace Factors
 {
     public class PlingPlangPlongTests
     {
-        
+
         [TestCase(34, "34")]
         [TestCase(1, "1")]
+        [TestCase(-1, "-1")]
         public void Given_AValueOf_WithNoneOfTheSpecifiedFactors_Returns_TheValueAsAString(int value, string expected)
         {
             var result = PlingPlangPlong.GetFactorResult(value);
@@ -22,6 +23,7 @@ namespace Factors
         }
 
         [TestCase(5, "Plang")]
+        [TestCase(-5, "Plang")]
         public void GivenAValueWithA_FactorOf5_AndNotOf3And7_Returns_Plang(int value, string expected)
         {
             var result = PlingPlangPlong.GetFactorResult(value);
@@ -29,7 +31,43 @@ namespace Factors
         }
 
         [TestCase(7, "Plong")]
+        [TestCase(28, "Plong")]
+        [TestCase(-28, "Plong")]
         public void GivenAValueWithA_FactorOf7_AndNotOf3And5_Returns_Plong(int value, string expected)
+        {
+            var result = PlingPlangPlong.GetFactorResult(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(30, "PlingPlang")]
+        [TestCase(-30, "PlingPlang")]
+        [TestCase(15, "PlingPlang")]
+        public void GivenAValueWithA_FactorOf3AND5_BUTNotOf7_Returns_PlingPlang(int value, string expected)
+        {
+            var result = PlingPlangPlong.GetFactorResult(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(35, "PlangPlong")]
+        [TestCase(-35, "PlangPlong")]
+        public void GivenAValueWithA_FactorOf5AND7_ButNotOf3_Returns_PlangPlong(int value, string expected)
+        {
+            var result = PlingPlangPlong.GetFactorResult(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(21, "PlingPlong")]
+        public void GivenAValueWithA_FactorOf3AND7_ButNotOf5_Returns_PlingPlong(int value, string expected)
+        {
+            var result = PlingPlangPlong.GetFactorResult(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(0, "PlingPlangPlong")]
+        [TestCase(105, "PlingPlangPlong")]
+        [TestCase(-105, "PlingPlangPlong")]
+
+        public void GivenAValueWithA_FactorOf3AND5_AND7_Returns_PlingPlangPlong(int value, string expected)
         {
             var result = PlingPlangPlong.GetFactorResult(value);
             Assert.That(result, Is.EqualTo(expected));
